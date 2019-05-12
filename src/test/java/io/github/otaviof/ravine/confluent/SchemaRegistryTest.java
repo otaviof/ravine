f8bcdecd72907f6eaa5b413fa37aebce99fa22a5
Otávio Fernandes <ofernandes@schubergphilis.com>
2019-05-12 08:55:59 +0200

    Fixing Schema-Registry tests.

--- src/test/java/io/github/otaviof/ravine/confluent/SchemaRegistryTest.java
+++ src/test/java/io/github/otaviof/ravine/confluent/SchemaRegistryTest.java
@@ -9,36 +9,36 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class SchemaRegistryTest {
+class SchemaRegistryTest {
     private final SchemaRegistry schemaRegistry = new SchemaRegistry("http://127.0.0.1:8681");
     private SubjectConfig subject;
 
     @BeforeAll
-    public static void waitForSchemaRegistry() throws IOException, RestClientException {
+    static void waitForSchemaRegistry() throws IOException, RestClientException {
         PrepareBackend.waitForSchemaRegistry();
     }
 
     @BeforeEach
-    public void prepare() {
+    void prepare() {
         subject = new SubjectConfig();
         subject.setName("person");
         subject.setVersion(1);
     }
 
     @Test
-    public void subjectExists() throws Exception {
+    void subjectExists() throws Exception {
         Assertions.assertThat(schemaRegistry.subjectExists(subject.getName())).isTrue();
     }
 
     @Test
-    public void latestVersion() throws Exception {
+    void latestVersion() throws Exception {
         Assertions.assertThat(1).isEqualTo(schemaRegistry.latestVersion(subject.getName()));
     }
 
     @Test
-    public void getSubject() throws Exception {
-        var schema = schemaRegistry.getSubject(subject.getName());
+    void getSubject() throws Exception {
+        var schemaData = schemaRegistry.getSubject(subject.getName());
 
-        Assertions.assertThat(schema).isEqualTo(schemaRegistry.getSubject(subject));
+        Assertions.assertThat(schemaData).isEqualTo(schemaRegistry.getSubject(subject));
     }
 }
